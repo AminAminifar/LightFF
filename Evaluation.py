80a72dc8dc6f3b753aa81736af4fbb261574615d
Amin Aminifar <43568226+AminAminifar@users.noreply.github.com>
2023-07-29 14:56:14 +0200

    Add files via upload

--- Evaluation.py
+++ Evaluation.py
@@ -14,13 +14,14 @@ def print_results(labels_vec, predictions_vec):
 
 def eval_train_set(model, inputs, targets):
     # train set
-    train_data_record_indices = range(0, 60000)
+    num_train_samples = 50000
+    train_data_record_indices = range(0, num_train_samples)
 
-    batch_size = 500
-    num_batches = int(60000 / batch_size)
+    batch_size = 5000
+    num_batches = int(num_train_samples / batch_size)
     chunk_indices = np.array_split(train_data_record_indices, num_batches)
 
-    y_predicted = np.zeros((60000))
+    y_predicted = np.zeros(num_train_samples)
     for i in range(num_batches):
         x_pos_ = inputs[chunk_indices[i]]
         y_predicted[chunk_indices[i]] = model.predict(x_pos_).detach().cpu().numpy()
@@ -32,12 +33,13 @@ def eval_train_set(model, inputs, targets):
 
 def eval_test_set(model, inputs, targets):
     # test set
-    test_data_record_indices = range(0, 10000)
+    num_test_samples = 10000
+    test_data_record_indices = range(0, num_test_samples)
 
-    batch_size = 500
-    num_batches = int(10000 / batch_size)
+    batch_size = 5000
+    num_batches = int(num_test_samples / batch_size)
     chunk_indices_test = np.array_split(test_data_record_indices, num_batches)
-    y_predicted = np.zeros((10000))
+    y_predicted = np.zeros(num_test_samples)
     for i in range(num_batches):
         x_pos_ = inputs[chunk_indices_test[i]]
         y_predicted[chunk_indices_test[i]] = model.predict(x_pos_).detach().cpu().numpy()
@@ -46,3 +48,20 @@ def eval_test_set(model, inputs, targets):
     print_results(targets.detach().cpu().numpy(), y_predicted)
     print('\tError:', 1.0 - torch.eq(torch.tensor(y_predicted), targets.detach().cpu()).float().mean().item())
 
+
+def eval_val_set(model, inputs, targets):
+    # test set
+    num_test_samples = 10000
+    test_data_record_indices = range(0, num_test_samples)
+
+    batch_size = 5000
+    num_batches = int(num_test_samples / batch_size)
+    chunk_indices_validation = np.array_split(test_data_record_indices, num_batches)
+    y_predicted = np.zeros(num_test_samples)
+    for i in range(num_batches):
+        x_pos_ = inputs[chunk_indices_validation[i]]
+        y_predicted[chunk_indices_validation[i]] = model.predict(x_pos_).detach().cpu().numpy()
+
+    print("\nResults for the {}VALIDATION{} set: ".format('\033[1m', '\033[0m'))
+    print_results(targets.detach().cpu().numpy(), y_predicted)
+    print('\tError:', 1.0 - torch.eq(torch.tensor(y_predicted), targets.detach().cpu()).float().mean().item())
