c3a70b5dd83ca996e421c934d4e167b411a24bdb
Baichuan <862843076@qq.com>
2023-09-01 10:40:57 +0200

    update_MNIST_CIFAR10

---
+++ compute goodness for every label/MNIST_layer_adjustable/Main.py
@@ -0,0 +1,102 @@
+import torch
+from torchvision.datasets import MNIST
+from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import Train
+import Evaluation
+
+from sklearn.model_selection import train_test_split
+
+import tools
+import os
+
+import numpy as np
+
+print('MNIST_Final_layer')
+layers = [784, 2000,2000,2000,2000]
+length_network = len(layers)-1
+print('layers: ' + str(length_network))
+Train_flag = True  # True False
+
+# load data
+def MNIST_loaders(train_batch_size=60000, test_batch_size=10000):
+    transform = Compose([
+        ToTensor(),
+        Normalize((0.1307,), (0.3081,)),
+        Lambda(lambda x: torch.flatten(x))])
+
+    train_loader = DataLoader(
+        MNIST('./data/', train=True,
+              download=True,
+              transform=transform),
+        batch_size=train_batch_size, shuffle=False)  # True
+
+    test_loader = DataLoader(
+        MNIST('./data/', train=False,
+              download=True,
+              transform=transform),
+        batch_size=test_batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
+
+def overlay_y_on_x(x, y):
+    """Replace the first 10 pixels of data [x] with one-hot-encoded label [y]
+    """
+    x_ = x.clone()
+    x_[:, :10] *= 0.0
+    x_[range(x.shape[0]), y] = x.max()
+    return x_
+
+
+train_loader, test_loader = MNIST_loaders()
+
+# train data
+inputs, targets = next(iter(train_loader))
+#inputs, targets = inputs.cuda(), targets.cuda()
+
+# create a validation set
+X_train, X_val, y_train, y_val = train_test_split(inputs, targets, test_size=10000, random_state=0)
+
+
+# train
+if Train_flag:
+
+    x_pos = overlay_y_on_x(X_train, y_train)
+    y_neg = y_train.clone()
+    for idx, y_samp in enumerate(y_train):
+        allowed_indices = [0, 1, 2, 3, 4,5,6,7,8,9]
+        allowed_indices.pop(y_samp.item())
+        y_neg[idx] = torch.tensor(np.random.choice(allowed_indices))
+    x_neg = overlay_y_on_x(X_train, y_neg)
+
+
+    model = Train.build_model(x_pos=x_pos, x_neg=x_neg,layers = layers)
+
+# Load the trained model from saved file
+# '4L_2kN_100E_500B' '2L_500N_100E_5000B' '4L_2000N_500E_5kB_50kS' '2L_500N_100E_5kB_50kS'
+#name = 'temp'
+#model = torch.load(os.path.split(os.path.realpath(__file__))[0]+'/model/' + name)
+
+# evaluation
+Evaluation.eval_train_set(model, inputs=X_train, targets=y_train)
+
+# test data
+x_te, y_te = next(iter(test_loader))
+#x_te, y_te = x_te.cuda(), y_te.cuda()
+
+Evaluation.eval_test_set(model, inputs=x_te, targets=y_te) 
+
+# validation data
+Evaluation.eval_val_set(model, inputs=X_val, targets=y_val)
+
+# analysis of validation data
+#tools.analysis_val_set_2l(model, inputs=X_val, targets=y_val)
+mean,std = tools.analysis_val_set(model, inputs=X_val, targets=y_val,length_network= length_network) # measure each layer's error/ based on the whole sample
+
+Evaluation.eval_val_set_light(model, inputs=X_val, targets=y_val,means = mean, stds = std,length_network= length_network) # measure how many layer needed/ based on each sample
+#Evaluation.eval_val_set_light(model, inputs=x_te, targets=y_te)  ## temporary use
+
+
+
---
+++ compute goodness for every label/MNIST_layer_adjustable/Train.py
@@ -0,0 +1,154 @@
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from torch.optim import Adam
+
+import numpy as np
+from sklearn.utils import shuffle
+
+import os
+
+def overlay_y_on_x(x, y):
+    """Replace the first 10 pixels of data [x] with one-hot-encoded label [y]
+    """
+    x_ = x.clone()
+    x_[:, :10] *= 0.0
+    x_[range(x.shape[0]), y] = x.max()
+    return x_
+
+
+class Net(torch.nn.Module):
+    def __init__(self, dims):
+        super().__init__()
+        self.layers = []
+        for d in range(len(dims) - 1):
+            #self.layers += [Layer(dims[d], dims[d + 1]).cuda()]
+            self.layers += [Layer(dims[d], dims[d + 1])]
+
+    def predict(self, x):
+        goodness_per_label = []
+        for label in range(10):
+            h = overlay_y_on_x(x, label)
+            goodness = []
+            for layer in self.layers:
+                h = layer(h)
+                goodness += [h.pow(2).mean(1)]
+            goodness_per_label += [sum(goodness).unsqueeze(1)]
+        goodness_per_label = torch.cat(goodness_per_label, 1)
+        return goodness_per_label.argmax(1)
+
+    def light_predict_one_sample(self, x,means,stds,length_network):
+        goodness_per_label = np.zeros(10)
+        h_list = []
+        for label in range(10):
+            h_list.append(overlay_y_on_x(x, label))
+
+        next_layer_flag = True
+        predicted_with_layers_up_to = 0
+        for i, layer in enumerate(self.layers, start=1):
+
+            for j in range(length_network):
+                if i == j+1 and next_layer_flag:
+                    for label in range(10):
+                        h_list[label] = layer(h_list[label])
+                        goodness_per_label[label] += h_list[label].pow(2).mean(1)
+                    predicted_with_layers_up_to += 1
+                    next_layer_flag = False
+                    if max(goodness_per_label) < (means[i-1] - stds[i-1] ):
+                        next_layer_flag = True
+
+        h_list.clear()
+        # print(goodness_per_label, predicted_with_layers_up_to)
+        # print(goodness_per_label.argmax(0))
+        return goodness_per_label.argmax(0), predicted_with_layers_up_to
+
+    def light_predict_4l(self, x,length_network):
+
+        goodness_per_label = [ [ ] for i in range(length_network) ]
+
+        for label in range(10):
+            h = overlay_y_on_x(x, label)
+
+            goodness = [ [ ] for i in range(length_network) ]
+            # confidence_threshold = xyz
+            for i, layer in enumerate(self.layers, start=1):
+
+                h = layer(h)
+                for j in range(i,length_network+1):
+                    goodness[j-1] +=[ h.pow(2).mean(1)]
+
+            for i in range(length_network):
+                goodness_per_label[i] += [sum(goodness[i]).unsqueeze(1)]  
+
+        goodness_final_label  = [ [ ] for i in range(length_network) ] 
+        for i in range(length_network):
+            goodness_per_label[i] = torch.cat(goodness_per_label[i], 1)
+            goodness_final_label[i] = goodness_per_label[i].argmax(1).unsqueeze(0)
+
+        #temp1 = torch.cat(goodness_final_label,0)
+        return torch.cat(goodness_final_label,0),goodness_per_label
+
+
+    def train(self, x_pos, x_neg):
+        h_pos, h_neg = x_pos, x_neg
+        for i, layer in enumerate(self.layers):
+            # print('training layer', i, '...')
+            h_pos, h_neg = layer.train(h_pos, h_neg)
+
+
+class Layer(nn.Linear):
+    def __init__(self, in_features, out_features,
+                 bias=True, device=None, dtype=None):
+        super().__init__(in_features, out_features, bias, device, dtype)
+        self.relu = torch.nn.ReLU()
+        self.opt = Adam(self.parameters(), lr=0.03)
+        self.threshold = 2.0
+        self.num_iterations = 1
+
+    def forward(self, x):
+        x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
+        return self.relu(
+            torch.mm(x_direction, self.weight.T) +
+            self.bias.unsqueeze(0))
+
+    def train(self, x_pos, x_neg):
+        for i in range(self.num_iterations):
+            g_pos = self.forward(x_pos).pow(2).mean(1)
+            g_neg = self.forward(x_neg).pow(2).mean(1)
+            # The following loss pushes pos (neg) samples to
+            # values larger (smaller) than the self.threshold.
+            loss = torch.log(1 + torch.exp(torch.cat([
+                -g_pos + self.threshold,
+                g_neg - self.threshold]))).mean()
+            self.opt.zero_grad()
+            # this backward just compute the derivative and hence
+            # is not considered backpropagation.
+            loss.backward()
+            self.opt.step()
+        return self.forward(x_pos).detach(), self.forward(x_neg).detach()
+
+def build_model(x_pos, x_neg,layers):
+    # torch.manual_seed(1234)
+
+
+    model = Net(layers) 
+
+    num_epochs = 100
+    for epoch in tqdm(range(num_epochs)):
+        num_train_samples = 50000  # 60000
+        train_data_record_indices = range(0, num_train_samples)
+        train_data_record_indices_shuffled = shuffle(train_data_record_indices)
+
+        batch_size = 500
+        num_batches = int(num_train_samples / batch_size)
+        chunk_indices = np.array_split(train_data_record_indices_shuffled, num_batches)
+        for i in range(num_batches):
+            x_pos_, x_neg_ = x_pos[chunk_indices[i]], x_neg[chunk_indices[i]]
+            model.train(x_pos_, x_neg_)
+
+    # save model
+    #name = 'temp'  # '4L_2kN_100E_500B' '2L_500N_100E_5kB_50kS' '2L_500N_10E_500B_50kS'
+    #torch.save(model, os.path.split(os.path.realpath(__file__))[0]+'/model/' + name)
+    return model
+
